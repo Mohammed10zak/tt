d931102c85252468c0767149e4ad9ca99d79578c
mohammed zakout <mohammedzakkh@gmail.com>
2022-12-14 13:09:25 +0300

    public aded

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import AboutPage from "./pages/About";
+import HomePage from "./pages/Home";
+
+function App() {
+  // const styles = {
+  // backgroundColor:'red',
+  // fontSize:100
+  // }
+  return (
+    <div className="App">
+      {/* <AboutPage /> */}
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <Header />
+        <h2>Home Page</h2>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default HomePage;
